694b164569bd7420cd1ad59a7b10fdfb21e4f014
prashant shrikant <prashantshrikant@mac.local>
2021-05-01 15:48:52 -0700

    adding knex framework

--- db/queries/queries.js
+++ db/queries/queries.js
@@ -1,7 +1,7 @@
-const db = require('../../lib/db');
+const knex = require('../../lib/db');
 
 const listListings = () => {
-  return db.query(`SELECT * FROM listings
+  return knex.schema.raw(`SELECT * FROM listings
     join resources on listings.resource_id = resources.id
     join users on listings.user_id = users.id;`)
     .then(res => {
