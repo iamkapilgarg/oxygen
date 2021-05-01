b9005930e436db80b1b0453b406de2f9788d0656
Nikhil Dominic <ndominic8555@gmail.com>
2021-05-01 16:51:55 -0700

    post listing done bitches

--- db/queries/queries.js
+++ db/queries/queries.js
@@ -1,14 +1,30 @@
-const knex = require('../../lib/db');
+const knex = require("../../lib/db");
 
 const listListings = () => {
-  return knex.schema.raw(`SELECT * FROM listings
+  return knex.schema
+    .raw(
+      `SELECT * FROM listings
     join resources on listings.resource_id = resources.id
-    join users on listings.user_id = users.id;`)
-    .then(res => {
+    join users on listings.user_id = users.id;`
+    )
+    .then((res) => {
       return res.rows;
-    })
-}
+    });
+};
+
+const postListing = (list) => {
+  console.log("List on the way ", list);
+
+  return knex("listings").insert({
+    user_id: list.user_id,
+    resource_id: list.resource_id,
+    quantity: list.quantity,
+    listing_type: list.listing_type,
+    oxygen_level: list.oxygen_level,
+  });
+};
 
 module.exports = {
-  listListings
-}
+  listListings,
+  postListing,
+};
--- routes/listings.js
+++ routes/listings.js
@@ -1,24 +1,27 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const router = express.Router();
-const { listListings } = require('../db/queries/queries')
-
+const { listListings, postListing } = require("../db/queries/queries");
 
 router.get("/", (req, res) => {
-  listListings().then(data => {
+  listListings().then((data) => {
     const templateVars = {
-      'data': data,
+      data: data,
     };
-    res.render('listings', templateVars);
+    res.render("listings", templateVars);
   });
 });
 
-router.post('/', (req, res) => {
-  console.log("req:",req);
-  const user = {
-    test : 'test'
-  }
-  res.status(201).json(req.body)
- })
- 
+router.post("/", (req, res) => {
+  postListing(req.body)
+    .then((data) => {
+      console.log(data.rows);
+      res.status(200);
+    })
+    .catch((err) => {
+      console.log("Error in posting:", err);
+      res.status(400).end();
+    });
+});
+
 module.exports = router;
