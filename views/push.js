7fd3b933674c29f7024967030ae738abd10d1f78
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-05-02 21:49:10 -0700

    notification

--- views/push.js
+++ views/push.js
@@ -1,4 +1,4 @@
-var push = require ('web-push')
+var push = require('web-push')
 
 var vapidKeys =
 {
@@ -6,8 +6,10 @@ var vapidKeys =
   privateKey: '4uDq3n6wETZ92CWFTFXctn7njCJhuLFyPyY9xXGpR2g'
 }
 
-push.setVapidDetails('mailto:test@test.com', vapidKeys.publicKey, vapidKeys.privateKey )
+push.setVapidDetails('mailto:test@test.com', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub = {}
+let sub = {
+  "endpoint" : "https://fcm.googleapis.com/fcm/send/fbSSjNtdKfo:APA91bG6bPBArhwGp-d82fMQ57B4bxLalM5fySp-1i892khabKUC_0XCg90eYuhbg5eZEJ2BqKEjLhsxVNoXvPxE7dwI10C1VokI62amOsbcS0LlcgTCo2VUBqWr-t0_BjZGMtZtIFBD",
+  "expirationTime": null, "keys": { "p256dh": "BIY_X60I9ki2HMag_A1niWu5PXmIyI9_-IHgud4lKQbqmaJjiVyASir9pjVSYd2ri8cMOENbnvwYl1CGgzvo4XA", "auth": "MGc95Pjxq6M1ruXy6RFrXQ" } }
 
-push.showNotification(sub, 'tst')
+push.sendNotification(sub, 'tst')
